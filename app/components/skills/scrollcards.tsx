d3f80831d4cc9c75d4e2f51c1a665f27559e46b5
Sadia Akter <aktersadiaa789@gmail.com>
2026-04-05 02:01:46 +0600

    Add gover font

--- app/components/skills/scrollcards.tsx
+++ app/components/skills/scrollcards.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useRef } from "react"
-import Lenis from "lenis"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { useGSAP } from "@gsap/react"
